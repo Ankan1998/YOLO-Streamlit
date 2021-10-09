eefa3913d644ae86162c087132da83a9d35c32c4
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-10-09 23:49:58 +0530

    Added confidence slider

--- app_streamlit_video.py
+++ app_streamlit_video.py
@@ -10,7 +10,16 @@ st.title("Object detection with YOLO")
 config_path = r'C:\Users\Ankan\Desktop\Github\FastAPI-model-serving\Inference\config.json'
 
 inf_pipeline = Inference_pipeline(config_path)
-selected = st.radio('Select type of File', ["Images","Video"])
+st.subheader("Select Image or Video")
+selected = st.radio("",["Images","Video"])
+st.subheader("Confidence")
+conf = st.slider("",
+                 min_value=0.0,
+                 max_value=1.0,
+                 step=0.1,
+                 value=0.5
+                 )
+
 if selected =="Video":
     video_file_buffer = st.file_uploader("Upload a Video", type=["mp4"])
     if video_file_buffer:
@@ -21,7 +30,7 @@ if selected =="Video":
         # if st.button("Detect Objects"):
         while True:
             ret, fr = video.read()
-            labels, nms_result, bboxes, conf,class_idx = inf_pipeline.inference_image(fr)
+            labels, nms_result, bboxes, conf,class_idx = inf_pipeline.inference_image(fr,conf)
             Object_detection_visualizer(fr, labels, nms_result, bboxes, conf, class_idx)
             img_ = Image.fromarray(cv2.cvtColor(fr, cv2.COLOR_BGR2RGB))
             stframe.image(img_, caption='Object Detection on Video')
@@ -32,7 +41,7 @@ else:
     if img_file_buffer:
         image_org = Image.open(img_file_buffer)
         img = cv2.cvtColor(np.array(image_org), cv2.COLOR_RGB2BGR)
-        labels, nms_result, bboxes, conf, class_idx = inf_pipeline.inference_image(img)
+        labels, nms_result, bboxes, conf, class_idx = inf_pipeline.inference_image(img,conf)
         Object_detection_visualizer(img, labels, nms_result, bboxes, conf, class_idx)
         img_ = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         st.image(img_, caption='Object Detection on Image')
