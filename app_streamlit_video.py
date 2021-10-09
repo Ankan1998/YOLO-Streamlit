65355d91190495e2de2c61308a510fb8f52ae371
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-10-09 23:38:03 +0530

    Video and Images integrated in streamlit app

--- app_streamlit_video.py
+++ app_streamlit_video.py
@@ -5,18 +5,36 @@ from PIL import Image
 from Inference.inference import Inference_pipeline
 from Inference.visualizer import Object_detection_visualizer
 import tempfile
+
 st.title("Object detection with YOLO")
 config_path = r'C:\Users\Ankan\Desktop\Github\FastAPI-model-serving\Inference\config.json'
+
 inf_pipeline = Inference_pipeline(config_path)
-video_file_buffer = st.file_uploader("Upload a Video", type=["mp4"])
-tfile = tempfile.NamedTemporaryFile(delete=False)
-tfile.write(video_file_buffer.read())
-video = cv2.VideoCapture(tfile.name)
-stframe = st.empty()
-if st.button("Detect Objects"):
-    while True:
-        ret, fr = video.read()
-        labels, nms_result, bboxes, conf,class_idx = inf_pipeline.inference_image(fr)
-        Object_detection_visualizer(fr, labels, nms_result, bboxes, conf, class_idx)
-        img_ = Image.fromarray(cv2.cvtColor(fr, cv2.COLOR_BGR2RGB))
-        stframe.image(img_, caption='Predicted Image.')
+selected = st.radio('Select type of File', ["Images","Video"])
+if selected =="Video":
+    video_file_buffer = st.file_uploader("Upload a Video", type=["mp4"])
+    if video_file_buffer:
+        tfile = tempfile.NamedTemporaryFile(delete=False)
+        tfile.write(video_file_buffer.read())
+        video = cv2.VideoCapture(tfile.name)
+        stframe = st.empty()
+        # if st.button("Detect Objects"):
+        while True:
+            ret, fr = video.read()
+            labels, nms_result, bboxes, conf,class_idx = inf_pipeline.inference_image(fr)
+            Object_detection_visualizer(fr, labels, nms_result, bboxes, conf, class_idx)
+            img_ = Image.fromarray(cv2.cvtColor(fr, cv2.COLOR_BGR2RGB))
+            stframe.image(img_, caption='Object Detection on Video')
+    else:
+        st.error("Please Upload a video")
+else:
+    img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+    if img_file_buffer:
+        image_org = Image.open(img_file_buffer)
+        img = cv2.cvtColor(np.array(image_org), cv2.COLOR_RGB2BGR)
+        labels, nms_result, bboxes, conf, class_idx = inf_pipeline.inference_image(img)
+        Object_detection_visualizer(img, labels, nms_result, bboxes, conf, class_idx)
+        img_ = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        st.image(img_, caption='Object Detection on Image')
+    else:
+        st.error("Please Upload a Image")
